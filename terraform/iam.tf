0599d25511f80b6f2ac83403d7d6ab5e6ce97fc0
luismotaiv <koke0mota@gmail.com>
2025-09-04 23:20:39 -0600

    FIX: add additional GitHub OIDC thumbprint for authentication

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -149,8 +149,10 @@ resource "aws_iam_openid_connect_provider" "github" {
     "sts.amazonaws.com",
   ]
 
+  # Thumbprints actualizados para GitHub Actions
   thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   ]
 
   tags = {
